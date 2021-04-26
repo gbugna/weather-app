0ce8d555392f87beaef91898c466711223bec529
Gustavo <gbugna@gmail.com>
2021-04-26 15:53:49 -0300

    add component folder

---
+++ src/components/WeatherCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function WeatherCard() {
+
+    let Apikey = "2f57fabc82091acfcbd28e8e9fe86c8c";
+
+
+    const [temperature, settemperature] = useState()
+
+
+    useEffect(() => {
+        effect
+        return () => {
+            cleanup
+        }
+    }, [input])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default WeatherCard
